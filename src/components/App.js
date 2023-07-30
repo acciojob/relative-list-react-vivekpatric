63118ebfa018434af13429d5cc5ad9319e0d7cf2
vivekpatric <patricleo223@gmail.com>
2023-07-30 21:43:53 +0530

    result

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const relatives = ['John', 'Emily', 'David', 'Sarah', 'Michael'];
 
-export default App
+  return (
+    <div>
+      <h1>Relative List</h1>
+      <ol id='relativeList' key="relativeList">
+        {relatives.map((relative, index) => (
+          <li id='relativeListItem1' key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
+
+
